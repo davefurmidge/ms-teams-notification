90cd75dc6e3fb889263e5a1b50e2cf9ca7671f55
Dave Furmidge <dave_furmidge@epam.com>
2023-09-22 10:11:08 +0100

    initial build

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,69 @@
-import * as core from '@actions/core'
-import { wait } from './wait'
+import * as core from '@actions/core';
+import { Octokit } from '@octokit/rest';
+import axios from 'axios';
+import moment from 'moment-timezone';
+import { createMessageCard } from './message-card';
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run(): Promise<void> {
+const escapeMarkdownTokens = (text: string) =>
+  text
+    .replace(/\n\ {1,}/g, '\n ')
+    .replace(/[_*|-#>]/g, (match) => `\\${match}`);
+
+async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const githubToken: string = core.getInput('github-token', { required: true });
+    const msTeamsWebhookUri: string = core.getInput('ms-teams-webhook-uri', {
+      required: true,
+    });
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const notificationSummary: string =
+      core.getInput('notification-summary') || 'GitHub Action Notification';
+    const notificationColor: string =
+      core.getInput('notification-color') || '0b93ff';
+    const timezone: string = core.getInput('timezone') || 'UTC';
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const timestamp: string = moment()
+      .tz(timezone)
+      .format('dddd, MMMM Do YYYY, h:mm:ss a z');
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const { GITHUB_REPOSITORY, GITHUB_SHA, GITHUB_RUN_ID, GITHUB_RUN_NUMBER } =
+      process.env;
+    const [owner, repo] = (GITHUB_REPOSITORY || '').split('/');
+    const sha: string = GITHUB_SHA || '';
+    const runId: string = GITHUB_RUN_ID || '';
+    const runNum: string = GITHUB_RUN_NUMBER || '';
+
+    const params = { owner, repo, ref: sha };
+    const repoName: string = `${params.owner}/${params.repo}`;
+    const repoUrl: string = `https://github.com/${repoName}`;
+
+    const octokit = new Octokit({ auth: `token ${githubToken}` });
+    const commit = await octokit.repos.getCommit(params);
+    const author = commit.data.author;
+
+    const messageCard = await createMessageCard(
+      notificationSummary,
+      notificationColor,
+      commit,
+      author,
+      runNum,
+      runId,
+      repoName,
+      sha,
+      repoUrl,
+      timestamp
+    );
+
+    console.log(messageCard);
+
+    const response = await axios.post(msTeamsWebhookUri, messageCard);
+
+    console.log(response);
+    core.debug(response.data);
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    console.error(error);
+    core.setFailed(error.message);
   }
 }
+
+run();
---
+++ src/message-card.ts
@@ -0,0 +1,68 @@
+export interface MessageCard {
+    '@type': string;
+    '@context': string;
+    summary: string;
+    themeColor: string;
+    title: string;
+    sections: MessageSection[];
+    potentialAction: MessageAction[];
+  }
+  
+  export interface MessageSection {
+    activityTitle: string;
+    activityImage: string;
+    activitySubtitle: string;
+  }
+  
+  export interface MessageAction {
+    '@context': string;
+    target: string[];
+    '@type': string;
+    name: string;
+  }
+  
+  export function createMessageCard(
+    notificationSummary: string,
+    notificationColor: string,
+    commit: any,
+    author: any,
+    runNum: string,
+    repoName: string,
+    sha: string,
+    repoUrl: string,
+    timestamp: string
+  ): MessageCard {
+    const avatar_url = author?.avatar_url || 'https://www.gravatar.com/avatar/05b6d8cc7c662bf81e01b39254f88a48?d=identicon';
+  
+    const messageCard: MessageCard = {
+      '@type': 'MessageCard',
+      '@context': 'https://schema.org/extensions',
+      summary: notificationSummary,
+      themeColor: notificationColor,
+      title: notificationSummary,
+      sections: [
+        {
+          activityTitle: `**CI #${runNum} (commit ${sha.substr(0, 7)})** on [${repoName}](${repoUrl})`,
+          activityImage: avatar_url,
+          activitySubtitle: `by ${commit.data.commit.author.name} [(@${author.login})](${author.html_url}) on ${timestamp}`,
+        },
+      ],
+      potentialAction: [
+        {
+          '@context': 'http://schema.org',
+          target: [`${repoUrl}/actions/runs`],
+          '@type': 'ViewAction',
+          name: 'View Workflow Runs',
+        },
+        {
+          '@context': 'http://schema.org',
+          target: [commit.data.html_url],
+          '@type': 'ViewAction',
+          name: 'View Commit Changes',
+        },
+      ],
+    };
+  
+    return messageCard;
+  }
+  
